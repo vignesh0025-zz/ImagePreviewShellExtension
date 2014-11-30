6b95d50405dc096f5ba5d8a8a7991788a36c9b41
vignesh0025@gmail.com <mythilY@Teja2>
2014-11-30 19:09:23 +0530

    Cleanups

---
+++ PreviewHandler/Class1.cs
@@ -0,0 +1,26 @@
+﻿using SharpShell.Attributes;
+using System;
+using System.Runtime.InteropServices;
+using SharpShell.SharpPreviewHandler;
+using System.Drawing;
+
+[ComVisible(true)]
+[COMServerAssociation(AssociationType.ClassOfExtension, ".tga")]
+[DisplayName("TGA Preview Handler")]
+[Guid("23592D34-78BA-4F43-AA6A-19716561C36C")]
+[PreviewHandler(DisableLowILProcessIsolation = false)]
+public class PreviewHandler : SharpPreviewHandler
+{
+        
+        protected override PreviewHandlerControl DoPreview()
+        {
+            var ctrl = new PreviewControl();
+            ctrl.DoPreview(SelectedFilePath);
+            ctrl.BackColor = Color.Green;
+            return ctrl;
+        }
+}
+
+
+
+
---
+++ PreviewHandler/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Windows.Forms;
+using SharpShell.SharpPreviewHandler;
+
+public partial class PreviewControl : PreviewHandlerControl
+{
+        public string Path;
+        public PreviewControl()
+        {
+            InitializeComponent();
+            Dock = DockStyle.Fill;
+        }
+
+        public void DoPreview(String path)
+        {
+            Paloma.TargaImage tga = new Paloma.TargaImage(Path);
+            PictureBox pictureBox1 = new PictureBox();
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.Image = tga.Image;
+            pictureBox1.Width = this.Width;
+            pictureBox1.Height = this.Width;
+
+            TabControl tb = new TabControl();
+
+            TabPage tp1 = new TabPage();
+            tp1.Text = "Properities";
+            tp1.AutoScroll = true;
+
+            tp1.Controls.Add(new Label
+            {
+                Text = "Width:     " + tga.Header.Width + " px",
+                Dock = DockStyle.Top
+            });
+            tp1.Controls.Add(new Label
+            {
+                Text = "Height:    " + tga.Header.Height + " px",
+                Dock = DockStyle.Top
+            });
+            tp1.Controls.Add(new Label
+            {
+                Text = "ImageType: " + tga.Header.ImageType.ToString() + " px",
+                Dock = DockStyle.Top
+            });
+            tp1.Controls.Add(new Label
+            {
+                Text = "Format:   " + tga.Format.ToString() + " px",
+                Dock = DockStyle.Top
+            });
+
+
+            tp1.Controls.Add(new Label
+            {
+                Text = "Width:   " + tga.Header.Width + " px",
+                Dock = DockStyle.Top
+            });
+
+            tb.TabPages.Add(tp1);
+            tb.Dock = DockStyle.Bottom;
+
+            Controls.Add(pictureBox1);
+            Controls.Add(tb);
+            
+            
+        }
+}
